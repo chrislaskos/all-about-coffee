46cb7c02c7cad8c92b5c17ab8ac994aec9c601d1
Chris Laskos <lxristos@hotmail.com>
2025-08-26 23:40:47 +0300

    Finalize templates

--- app/routes.php
+++ app/routes.php
@@ -8,31 +8,45 @@ use Slim\App;
 use Slim\Views\PhpRenderer;
 
 return function (App $app) {
-    $app->get('/', function (Request $request, Response $response, $args) {
+    $app->get('/', function (Request $request, Response $response, array $args) {
         $renderer = new PhpRenderer(__DIR__ . '/../templates', ['title' => 'All About Coffee']);
         $renderer->setLayout('layout.php');
         
         return $renderer->render($response, 'home.php');
     })->setName('home');
 
-    $app->get('/brewing', function (Request $request, Response $response, $args) {
+    $app->get('/brewing', function (Request $request, Response $response, array $args) {
         $renderer = new PhpRenderer(__DIR__ . '/../templates', ['title' => 'Coffee Brewing']);
         $renderer->setLayout('layout.php');
 
         return $renderer->render($response, 'brewing.php');
     })->setName('brewing');
 
-    $app->get('/grinding', function (Request $request, Response $response, $args) {
+    $app->get('/grinding', function (Request $request, Response $response, array $args) {
         $renderer = new PhpRenderer(__DIR__ . '/../templates', ['title' => 'Coffee Grinding']);
         $renderer->setLayout('layout.php');
 
         return $renderer->render($response, 'grinding.php');
     })->setName('grinding');
 
-    $app->get('/roasting', function (Request $request, Response $response, $args) {
+    $app->get('/roasting', function (Request $request, Response $response, array $args) {
         $renderer = new PhpRenderer(__DIR__ . '/../templates', ['title' => 'Coffee Roasting']);
         $renderer->setLayout('layout.php');
 
         return $renderer->render($response, 'roasting.php');
     })->setName('roasting');
+
+    $app->get('/survey', function (Request $request, Response $response, array $args) {
+        $renderer = new PhpRenderer(__DIR__ . '/../templates', ['title' => 'Coffee Survey']);
+        $renderer->setLayout('layout.php');
+
+        return $renderer->render($response, 'survey.php');
+    })->setName('survey');
+
+    $app->get('/drinks', function (Request $request, Response $response, array $args) {
+        $renderer = new PhpRenderer(__DIR__ . '/../templates', ['title' => 'Coffee Drinks']);
+        $renderer->setLayout('layout.php');
+
+        return $renderer->render($response, 'drinks.php');
+    })->setName('drinks');
 };
---
+++ templates/survey.php
@@ -0,0 +1,40 @@
+<h1><?=$title?></h1>
+<aside>
+	<figure>
+		<img src="/assets/images/espresso.jpg" alt="a cup of espresso"/>
+	</figure>
+</aside>
+<form action="/survey.php" method="post">
+	<fieldset>
+		<legend>Personal Information</legend>
+		Your Name<br/>
+		<input type="text" name="name" required size="40"/>
+		<br/>
+		Email Address<br/>
+		<input type="email" name="email" required placeholder="enter a valid email address" size="40"/>
+		<br/>
+	</fieldset>
+	<fieldset>
+		<legend>Your Coffee Preferences</legend>
+		Tell us your coffee preparation methods.<br/>
+		<textarea name="preparation" cols="40" rows="6"></textarea>
+		<br/><br/>
+		Do you grind your own beans?<br/>
+		<input type="radio" name="grind" value="yes" />yes<br/>
+		<input type="radio" name="grind" value="no" />no<br/>
+
+		How do you drink your coffee?<br/>
+		<input type="checkbox" name="drink" value="sugar" />with sugar<br/>
+		<input type="checkbox" name="drink" value="cream" />with cream<br/>
+		<br/>
+		What is your favorite coffee drink?<br/>
+		<select name="favorite">
+		<option value="">please select a drink</option>
+		<option value="drip">drip coffee</option>
+		<option value="french">french press</option>
+		<option value="espresso">espresso</option>
+		<option value="latte">latte</option>
+		<option value="flavored">flavored</option>
+		</select>
+	</fieldset>
+</form>
