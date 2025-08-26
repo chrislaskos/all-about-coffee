a31250425777751a8e1bc14a519a14b82c752a4f
Chris Laskos <lxristos@hotmail.com>
2025-08-26 21:55:23 +0300

    Move to assets dir, set up homepage and layout, rename docker service

--- public/index.php
+++ public/index.php
@@ -3,14 +3,17 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Slim\Views\PhpRenderer;
 
 require __DIR__ . '/../vendor/autoload.php';
 
 $app = AppFactory::create();
 
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
-    return $response;
-});
+    $renderer = new PhpRenderer(__DIR__ . '/../templates', ['title' => 'All About Coffee']);
+    $renderer->setLayout('layout.php');
+    
+    return $renderer->render($response, 'home.php');
+})->setName('home');
 
 $app->run();
---
+++ templates/layout.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title><?=$title?></title>
+		<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+		<link href="/assets/styles.css" rel="stylesheet" type="text/css"/>
+        <link rel="shortcut icon" href="/assets/images/favicon.png" type="image/x-icon">
+	</head>
+	<body>
+        <header>
+            <h1>All About Coffee</h1>
+			<nav>
+				<ul>
+					<li><a href="/">Home</a></li>
+					<li><a href="roasting.html">Roasting</a></li>
+					<li><a href="grinding.html">Grinding</a></li>
+					<li><a href="brewing.html">Brewing</a></li>
+					<li><a href="drinks.html">Drinks</a></li>
+					<li><a href="survey.html">Coffe Survey</a></li>
+				</ul>
+			</nav>
+		</header>
+        <?=$content?>
+        <footer>
+            <nav>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="roasting.html">Roasting</a></li>
+                    <li><a href="grinding.html">Grinding</a></li>
+                    <li><a href="brewing.html">Brewing</a></li>
+                    <li><a href="drinks.html">Drinks</a></li>
+                    <li><a href="survey.html">Coffe Survey</a></li>
+                </ul>
+            </nav>
+		</footer>
+	</body>
+</html>
